ed6985cd27dadf0efb2ef7a20f2568bafeb7fd34
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-12-31 07:20:07 +0800

    Add CSS for Smartphone

--- my.js
+++ my.js
@@ -1,9 +1,10 @@
 
-import changeDaysStyleOnCLick , {changeOverlayIfDayOrNight} from "./style.js"
+import changeDaysStyleOnCLick, { changeOverlayIfDayOrNight } from "./style.js"
 
 async function getWeatherForHeader(city) {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b9d5054bdd6e474c906163150232512&q=${city}&aqi=no`)
     const data = await response.json()
+    console.log(data)
     const countryName = data.location.country
     const isDay = data.current.is_day
     const icon = data.current.condition.icon
@@ -17,11 +18,12 @@ async function getWeatherForHeader(city) {
     const wind = data.current.wind_kph
     const temperature = Math.round(data.current.temp_c)
     const feelsLike = data.current.feelslike_c
-    return { isDay,icon,countryName, regionName,cityName, localTime, description, humidity, rain, wind, temperature, feelsLike }
+    console.log("getWeatherForHeader")
+
+    return { isDay, icon, countryName, regionName, cityName, localTime, description, humidity, rain, wind, temperature, feelsLike }
 }
 
 async function addToHeaderDomOnSearch(input) {
-    console.log(input)
     const weatherObj = await getWeatherForHeader(input)
     const country = document.getElementById('country')
     const city = document.getElementById('city')
@@ -39,22 +41,22 @@ async function addToHeaderDomOnSearch(input) {
     temperature.innerText = weatherObj.temperature
     localTime.innerText = weatherObj.localTime
     mainIcon.src = weatherObj.icon
-    precipitation.innerText = weatherObj.rain +'mm'
-    humidity.innerText = weatherObj.humidity +'%'
-    wind.innerText = weatherObj.wind +' kph'
+    precipitation.innerText = weatherObj.rain + 'mm'
+    humidity.innerText = weatherObj.humidity + '%'
+    wind.innerText = weatherObj.wind + ' kph'
+    console.log("addToHeaderDomOnSearch")
 }
 
-async function getFutureForecast(city){
+async function getFutureForecast(city) {
     const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=b9d5054bdd6e474c906163150232512&q=${city}&days=7&aqi=no&alerts=no`)
     const data = await response.json()
     const forecastArray = data.forecast.forecastday
     return forecastArray
 }
 
-async function addFutureForecastToDom(city){
+async function addFutureForecastToDom(city) {
     const forecastArray = await getFutureForecast(city)
-    console.log(forecastArray)
-    for (let i = 1; i <= forecastArray.length; i++){
+    for (let i = 1; i <= forecastArray.length; i++) {
         const day = document.getElementById(`dayName${i}`)
         const weatherIcon = document.getElementById(`icon${i}`)
         const celsiusTemp = document.querySelector(`.mini-celsius${i}`)
@@ -64,23 +66,24 @@ async function addFutureForecastToDom(city){
         const miniHumid = document.getElementsByClassName(`mini-humid`)
         const miniWind = document.getElementsByClassName(`mini-wind`)
         const miniDescription = document.getElementsByClassName(`mini-description`)
-        let dateName = new Date(forecastArray[i-1].date)
+        let dateName = new Date(forecastArray[i - 1].date)
         let today = dateName.getDay()
         let dayInLetters = switchDayNumberToName(today)
 
-        miniPrep[i-1].innerText = forecastArray[i-1].day.totalprecip_mm
-        miniHumid[i-1].innerText = forecastArray[i-1].day.avghumidity
-        miniWind[i-1].innerText = forecastArray[i-1].day.avgvis_km
-        miniDescription[i-1].innerText = forecastArray[i-1].day.condition.text
+        miniPrep[i - 1].innerText = forecastArray[i - 1].day.totalprecip_mm
+        miniHumid[i - 1].innerText = forecastArray[i - 1].day.avghumidity
+        miniWind[i - 1].innerText = forecastArray[i - 1].day.avgvis_km
+        miniDescription[i - 1].innerText = forecastArray[i - 1].day.condition.text
         day.innerText = dayInLetters
-        date.innerText = forecastArray[i-1].date
-        weatherIcon.src = forecastArray[i-1].day.condition.icon
-        celsiusTemp.innerText = Math.round(forecastArray[i-1].day.avgtemp_c)
-        ferTemp.innerText = Math.round(forecastArray[i-1].day.avgtemp_f)
+        date.innerText = forecastArray[i - 1].date
+        weatherIcon.src = forecastArray[i - 1].day.condition.icon
+        celsiusTemp.innerText = Math.round(forecastArray[i - 1].day.avgtemp_c)
+        ferTemp.innerText = Math.round(forecastArray[i - 1].day.avgtemp_f)
     }
+    console.log('addFutureForecastToDom')
 }
 
-async function updateHeaderWhenDayIsClicked(day){
+async function updateHeaderWhenDayIsClicked(day) {
     //Targeted elements
     const localTime = document.getElementById('time')
     const precipitation = document.getElementById('precipitation')
@@ -95,43 +98,44 @@ async function updateHeaderWhenDayIsClicked(day){
     precipitation.innerText = document.getElementById(`mini-prep${day}`).innerText
     humidity.innerText = document.getElementById(`mini-humid${day}`).innerText
     wind.innerText = document.getElementById(`mini-wind${day}`).innerText
-    description.innerText = allMiniDescriptions[day-1].innerText
+    description.innerText = allMiniDescriptions[day - 1].innerText
     temperature.innerText = document.querySelector(`.mini-celsius${day}`).innerText
     localTime.innerText = document.getElementById(`date${day}`).innerText
 }
 
-function clickSpecificDay(){
+function clickSpecificDay() {
     const allDays = document.getElementsByClassName('day')
-    for (let i = 0; i < allDays.length ; i++){
+    for (let i = 0; i < allDays.length; i++) {
         allDays[i].addEventListener('click', () => {
-            updateHeaderWhenDayIsClicked(i+1)
+            updateHeaderWhenDayIsClicked(i + 1)
         })
     }
+    console.log('clickSpecificDay')
 }
 
-function switchDayNumberToName(date){
-    switch (date){
+function switchDayNumberToName(date) {
+    switch (date) {
         case 0:
             return 'Mon'
-        break;
+            break;
         case 1:
             return 'Tue'
-        break;
+            break;
         case 2:
             return 'Wed'
-        break;
+            break;
         case 3:
             return 'Thur'
-        break;
+            break;
         case 4:
             return 'Fri'
-        break;
+            break;
         case 5:
             return 'Sat'
-        break;
+            break;
         case 6:
             return 'Sun'
-        break;
+            break;
     }
 }
 
@@ -141,6 +145,7 @@ window.addEventListener('load', () => {
     addFutureForecastToDom('Kenitra')
     clickSpecificDay()
     changeOverlayIfDayOrNight('Kenitra')
+    changeBadIcons()
 })
 
 //Load searched city forecast
@@ -150,7 +155,20 @@ button.addEventListener('click', () => {
     addToHeaderDomOnSearch(input.value)
     addFutureForecastToDom(input.value)
     changeOverlayIfDayOrNight(input.value)
-    }
+    input.value = ''
+}
 )
 
 changeDaysStyleOnCLick()
+
+async function changeBadIcons() {
+    const description = document.getElementById('description').innerText
+    console.log(description)
+    const mainIcon = document.getElementById('icon-temp')
+    console.log(1)
+    if (description === 'Clear') {
+        console.log(2)
+        mainIcon.src = 'img/clear-sky.svg'
+    }
+    console.log('changeBadIcons')
+}
--- style.js
+++ style.js
@@ -15,12 +15,9 @@ export async function changeOverlayIfDayOrNight(city){
     const data = await response.json()
 
     const isDay = data.location.localtime
-    console.log(isDay)
-    console.log(typeof isDay)
     
     const result = isDay.match((/ .*:/))
     const hourString = result[0].slice(0,-1)
-    console.log(hourString)
     const root = document.querySelector(':root');
     const morning = document.getElementById('morning')
     const night = document.getElementById('night')
@@ -29,23 +26,22 @@ export async function changeOverlayIfDayOrNight(city){
 
     
     if (hourString < 12 && hourString > 6){
-        console.log('Morning')
         root.style.setProperty("--overLayColor", "linear-gradient(to bottom, #fde68a 0%, #f7a15b 100%)");
         morning.style.transform = 'translateX(0%) translateY(0%)'
         afternoon.style.transform = 'translateX(-110%) translateY(-50%)'
         night.style.transform = 'translateX(-110%) translateY(-50%)'
     }
     else if (hourString > 12 && hourString < 18){
-        console.log('Afternoon')
         root.style.setProperty("--overLayColor", "linear-gradient(to bottom, #d97706 0%, #0e7490 100%)");
         morning.style.transform = 'translateX(-110%) translateY(-50%)'
         afternoon.style.transform = 'translateX(0%) translateY(0%)'
         night.style.transform = 'translateX(-110%) translateY(-50%)'
     }else{
-        console.log('Night')
         root.style.setProperty("--overLayColor", "linear-gradient(to bottom, #94a3b8 0%, #475569 100%)");
         morning.style.transform = 'translateX(-110%) translateY(-50%)'
         afternoon.style.transform = 'translateX(-110%) translateY(-50%)'
         night.style.transform = 'translateX(0%) translateY(0%)'
     }
+    console.log('changeOverlayIfDayOrNight')
 }
+
